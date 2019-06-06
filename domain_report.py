ef10079a7773e9b66d7361f2c5bbf4f544e7d40c
rajendraboke12 <51116848+rajendraboke12@users.noreply.github.com>
2019-06-06 18:07:00 +0530

    Update domain_report.py

--- domain_report.py
+++ domain_report.py
@@ -1,44 +1,48 @@
 import requests
 import yaml
 from pprint import pprint
+import os
 
-with open("/home/rajendra/Desktop/info.yml", 'r') as ymlfile:
+path=os.environ["WORKDIR"]
+with open(path+"/lookup_plugins/virustotal/info.yml", 'r') as ymlfile:
     info = yaml.load(ymlfile)
 
 def get_domain_report(inward_arr,var_arr):
-    outward_arr=[]
     for i in inward_arr:
         if (var_arr[0] in i):
             domainname=i[var_arr[0]]
             url = 'https://www.virustotal.com/vtapi/v2/domain/report'
-            params = {'apikey': info[0]["apikey"],'domain':domainname}
+            params = {'apikey': info["apikey"],'domain':domainname}
             response = requests.get(url, params=params)
             resp = response.json()
             try:
-                if "detected_communicating_samples" in resp:
-                    for j in resp["detected_communicating_samples"]:
-                        try:
-                            j["$name"]=info[0]["name"]
-                        except:
-                            j["$name"]= ""
-                        try:
-                            j["$phone_no"]=info[0]["phone_no"]
-                        except:
-                            j["$phone_no"]= 0
-                        try:
-                            j["$whois"]=resp["whois"]
-                        except:
-                            j["$whois"]=""
-                        try:
-                            j["$Alexa domain info"]=resp["Alexa domain info"]
-                        except:
-                            j["$Alexa domain info"]=""
-                    outward_arr.append(resp["detected_communicating_samples"])
-                else:
-                    outward_arr.append([])
-
+                i["$name"]=info["name"].upper()
             except:
-                pass
+                i["$name"]= ""
+            try:
+                i["$phone_no"]=info["phone_no"]
+            except:
+                i["$phone_no"]= 0
+            try:
+                i["$Alexa domain info"]=info["Alexa domain info"]
+            except:
+                i["$Alexa domain info"]= ""
+            try:
+                i["$BitDefender category"]=info["BitDefender category"]
+            except:
+                i["$BitDefender category"]= ""
+            try:
+                i["$BitDefender domain info"]=info["BitDefender domain info"]
+            except:
+                i["$BitDefender domain info"]= ""
+            try:
+                i["$Dr.Web category"]=info["Dr.Web category"]
+            except:
+                i["$Dr.Web category"]= ""
+            for j in resp:
+                if(isinstance(resp[j], unicode)):
+                    i["$"+str(j)]=resp[j]
+    outward_arr=inward_arr
     return outward_arr
 
 inward_arr = [{"domain":"netmonastery.com"},{"domain":"google.com"},{"domain":"gmail.com"}]
